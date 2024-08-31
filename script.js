69bc89b7af35faf3b1dcafcaac0bb048d12bac63
valentim18ludy <ludymilla.costa@sou.ufac.br>
2024-08-31 15:21:07 -0300

    feat: <Como proprietário eu quero que o erro no calculo imc seja corrigido> .Ref #5

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ function calculandoIMC(){
         let imc = (peso / (altura*altura)).toFixed(2);
         let mensagem = "";
         if(imc < 18.5){ mensagem = "Abaixo do peso!"
-        }else if(imc < 25){ ensagem = "Você está com o peso ideal!"
+        }else if(imc < 25){ mensagem = "Você está com o peso ideal!"
         }else if(imc < 30){ mensagem = "Você está levemente acima do peso!"
-        }else if(imc > 35){ mensagem = "Cuidado! Obesidade grau I"
-        }else if(imc > 40){mensagem = "Cuidado! Obesidade grau II"
+        }else if(imc < 35){ mensagem = "Cuidado! Obesidade grau I"
+        }else if(imc < 40){mensagem = "Cuidado! Obesidade grau II"
         }else { mensagem = "Cuidado! Obesidade grau III"
         }
         resultado.textContent = `Seu IMC é ${imc}! ${mensagem}`;      
